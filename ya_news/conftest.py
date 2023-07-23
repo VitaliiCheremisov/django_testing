bc7983c689f46a850df478f8719b9d4025319575
Виталий Черемисов <109948375+VitaliiCheremisov@users.noreply.github.com>
2023-07-23 21:33:23 +0300

    Add files via upload

---
+++ ya_news/conftest.py
@@ -0,0 +1,87 @@
+from datetime import datetime, timedelta
+
+import pytest
+
+from news.models import News, Comment
+from django.conf import settings
+from django.utils import timezone
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    author_client = client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def news(author):
+    news = News.objects.create(
+        title='Заголовок',
+        text='Текст новости',
+    )
+    return news
+
+
+@pytest.fixture
+def id_for_args(news):
+    return news.id,
+
+
+@pytest.fixture
+def comment(author, news):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def comment_id_for_args(comment):
+    return comment.id,
+
+
+@pytest.fixture
+def all_news():
+    today = datetime.today()
+    all_news = [
+        News(
+            title=f'Новость {index}',
+            text='Просто текст',
+            date=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+    ]
+    News.objects.bulk_create(all_news)
+    return all_news
+
+
+@pytest.fixture
+def more_news(author):
+    more_news = News.objects.create(
+        title='Тестовяа новость',
+        text='Просто текст'
+    )
+    now = timezone.now()
+    for index in range(2):
+        comment = Comment.objects.create(
+            news=more_news,
+            author=author,
+            text=f'Текст {index}'
+        )
+        comment.created = now + timedelta(days=index)
+        comment.save()
+    return more_news
+
+
+@pytest.fixture
+def form_data():
+    return {
+        'text': 'Новый текст'
+    }
