f0e2d713d6d46604cdfdea8daa974fdd953a229e
Виталий Черемисов <109948375+VitaliiCheremisov@users.noreply.github.com>
2023-07-23 21:32:10 +0300

    Add files via upload

---
+++ ya_news/news/pytest_tests/test_content.py
@@ -0,0 +1,49 @@
+import pytest
+
+from django.conf import settings
+from django.urls import reverse
+
+
+def test_news_count(client, all_news):
+    home_url = reverse('news:home')
+    response = client.get(home_url)
+    object_list = response.context['object_list']
+    news_count = len(object_list)
+    assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
+
+
+@pytest.mark.django_db
+def test_news_order(all_news, client):
+    home_url = reverse('news:home')
+    response = client.get(home_url)
+    object_list = response.context['object_list']
+    all_dates = [all_news.date for all_news in object_list]
+    sorted_dates = sorted(all_dates, reverse=True)
+    assert all_dates == sorted_dates
+
+
+def test_comments_order(more_news, client):
+    detail_url = reverse('news:detail', args=(more_news.id,))
+    response = client.get(detail_url)
+    assert 'news' in response.context
+    news = response.context['news']
+    all_comments = news.comment_set.all()
+    assert all_comments[0].created < all_comments[1].created
+
+
+def test_anonymous_client_has_no_form(client, more_news):
+    detail_url = reverse('news:detail', args=(more_news.id,))
+    response = client.get(detail_url)
+    assert 'form' not in response.context
+
+
+@pytest.mark.parametrize(
+    'parametrized_client',
+    (
+        (pytest.lazy_fixture('author_client'),)
+    )
+)
+def test_authorized_client_has_form(client, more_news, parametrized_client):
+    detail_url = reverse('news:detail', args=(more_news.id,))
+    response = parametrized_client.get(detail_url)
+    assert 'form' in response.context
