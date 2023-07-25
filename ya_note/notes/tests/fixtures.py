a86c7d5f1a7e05d0e28204a0ab5083b2e3d9e9a0
Виталий Черемисов <109948375+VitaliiCheremisov@users.noreply.github.com>
2023-07-25 15:26:15 +0300

    Add files via upload

---
+++ ya_note/notes/tests/fixtures.py
@@ -0,0 +1,17 @@
+class TestsUrls:
+    ANONYMOUS = (
+        'notes:home',
+        'users:login',
+        'users:logout',
+        'users:signup'
+    )
+    AUTHS = (
+        'notes:list',
+        'notes:add',
+        'notes:success'
+    )
+    AVAILAIBLE_DIFF_USERS = (
+        'notes:detail',
+        'notes:edit',
+        'notes:delete'
+    )
